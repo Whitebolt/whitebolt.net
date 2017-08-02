d348f00774d4e457d789d473f87e8f94fa1a44fb
Stephen <stephen@whitebolt.net>
2017-08-02 11:33:27 +0100

    Ensure LF not CRLf throughout
    Some windows formatted files had crept in somehow, this fixes that.

--- src/scripts/body/index.js
+++ src/scripts/body/index.js
@@ -5,21 +5,8 @@
 
 	angular.module("wb").directive("body", [
 		"boltDirective",
-	($directive) => {
+	($directive)=>{
 
-		/**
-		 * @description
-		 * Initial linking of directive.  This will add watches and some of the
-		 * controller attributes.
-		 *
-		 * @private
-		 * @param {Object} scope			AngularJs scope object.
-		 * @param {Object} root				jQuery node for the root of directive
-		 * 									(ie. the calling, initialising element).
-		 * @param {Object} attributes		AngularJs attributes object for the
-		 * 									calling element.
-		 * @param {Object} controller		AngularJs controller instance.
-		 */
 		function link(scope, root, attributes, controller) {
 			$directive.link({scope, root, controller});
 		}
